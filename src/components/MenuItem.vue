2b0659553862d0c84f6c9bed17024fb8193b0e80
vedil <alouan_adil@hotmail.fr>
2021-04-17 21:20:27 +0000

    'part3chapter2'

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="menu-item">
+    <img class="menu-item__image" :src="image.source" :alt="image.alt"/>
+    <div>
+      <h3>{{ name }}</h3>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        <input v-model.number="this.$props.quantity" id="add-item-quantity" type="number"/>
+        <button @click="addToShoppingCart(quantity)">Ajouter au panier d'achat</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "name", "inStock", "quantity"],
+
+}
+</script>
